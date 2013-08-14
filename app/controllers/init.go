4d3958338a60c17e3e1cd6f881fa759c95a59bbf
Karan Misra <karan@erodov.com>
2013-08-15 02:14:28 +0530

    init.go: set GOMAXPROCS

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -1,8 +1,13 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+	"github.com/robfig/revel"
+	"runtime"
+)
 
 func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	revel.OnAppStart(Init)
 	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
 	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
