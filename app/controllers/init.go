11f9a9173bfc31b0ed7b652ee0f90bbaeae26b99
Karan Misra <karan@erodov.com>
2013-09-14 02:36:20 +0530

    move the GOMAXPROCS init code outside the controllers dir

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -2,15 +2,9 @@ package controllers
 
 import (
 	r "github.com/robfig/revel"
-	"runtime"
 )
 
 func init() {
-	numCPU := runtime.NumCPU()
-	gomaxprocs := runtime.GOMAXPROCS(numCPU)
-
-	r.WARN.Printf("Total CPU detected %v, setting GOMAXPROCS to %v", numCPU, gomaxprocs)
-
 	r.InterceptMethod((*XormController).Begin, r.BEFORE)
 	r.InterceptMethod((*XormController).Commit, r.AFTER)
 	r.InterceptMethod((*XormController).Rollback, r.FINALLY)
--- app/init.go
+++ app/init.go
@@ -1,19 +1,30 @@
 package app
 
-import "github.com/robfig/revel"
+import (
+	r "github.com/robfig/revel"
+	"runtime"
+)
+
+func Init() {
+	numCPU := runtime.NumCPU()
+	gomaxprocs := runtime.GOMAXPROCS(numCPU)
+
+	r.WARN.Printf("GOMAXPROCS was %v, Total CPU detected %v, set as new GOMAXPROCS", gomaxprocs, numCPU)
+}
 
 func init() {
-	// Filters is the default set of global filters.
-	revel.Filters = []revel.Filter{
-		revel.PanicFilter,             // Recover from panics and display an error page instead.
-		revel.RouterFilter,            // Use the routing table to select the right Action
-		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-		revel.ParamsFilter,            // Parse parameters into Controller.Params.
-		revel.SessionFilter,           // Restore and write the session cookie.
-		revel.FlashFilter,             // Restore and write the flash cookie.
-		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		revel.I18nFilter,              // Resolve the requested language
-		revel.InterceptorFilter,       // Run interceptors around the action.
-		revel.ActionInvoker,           // Invoke the action.
+	r.Filters = []r.Filter{
+		r.PanicFilter,             // Recover from panics and display an error page instead.
+		r.RouterFilter,            // Use the routing table to select the right Action
+		r.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+		r.ParamsFilter,            // Parse parameters into Controller.Params.
+		r.SessionFilter,           // Restore and write the session cookie.
+		r.FlashFilter,             // Restore and write the flash cookie.
+		r.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+		r.I18nFilter,              // Resolve the requested language
+		r.InterceptorFilter,       // Run interceptors around the action.
+		r.ActionInvoker,           // Invoke the action.
 	}
+
+	r.OnAppStart(Init)
 }
