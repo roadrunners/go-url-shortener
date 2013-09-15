a8931f360c90f5b31571ea626266430b2d87373c
Karan Misra <karan@erodov.com>
2013-09-15 06:00:06 +0530

    redis.go: r. -> revel.

--- app/redis/redis.go
+++ app/redis/redis.go
@@ -2,7 +2,7 @@ package redis
 
 import (
 	"github.com/hoisie/redis"
-	r "github.com/robfig/revel"
+	"github.com/robfig/revel"
 )
 
 var (
@@ -12,17 +12,17 @@ var (
 func Init() {
 	var found bool
 	var addr string
-	if addr, found = r.Config.String("redis.addr"); !found {
-		r.ERROR.Fatal("No redis.addr found")
+	if addr, found = revel.Config.String("redis.addr"); !found {
+		revel.ERROR.Fatal("No redis.addr found")
 	}
 	var db int
-	if db, found = r.Config.Int("redis.db"); !found {
-		r.ERROR.Fatal("No redis.db found")
+	if db, found = revel.Config.Int("redis.db"); !found {
+		revel.ERROR.Fatal("No redis.db found")
 	}
-	r.INFO.Printf("Connecting to redis db %v at %v", db, addr)
+	revel.INFO.Printf("Connecting to redis db %v at %v", db, addr)
 	Client = &redis.Client{Addr: addr, Db: db}
 }
 
 func init() {
-	r.OnAppStart(Init)
+	revel.OnAppStart(Init)
 }
