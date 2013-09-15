17c02f800889c9f024b6f22ff5b183be4ea1e1b7
Karan Misra <karan@erodov.com>
2013-09-15 06:00:22 +0530

    init.go: r. -> revel.

--- app/init.go
+++ app/init.go
@@ -1,7 +1,7 @@
 package app
 
 import (
-	r "github.com/robfig/revel"
+	"github.com/robfig/revel"
 	"runtime"
 )
 
@@ -9,22 +9,22 @@ func Init() {
 	numCPU := runtime.NumCPU()
 	gomaxprocs := runtime.GOMAXPROCS(numCPU)
 
-	r.WARN.Printf("GOMAXPROCS was %v, Total CPU detected %v, set as new GOMAXPROCS", gomaxprocs, numCPU)
+	revel.WARN.Printf("GOMAXPROCS was %v, Total CPU detected %v, set as new GOMAXPROCS", gomaxprocs, numCPU)
 }
 
 func init() {
-	r.Filters = []r.Filter{
-		r.PanicFilter,             // Recover from panics and display an error page instead.
-		r.RouterFilter,            // Use the routing table to select the right Action
-		r.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-		r.ParamsFilter,            // Parse parameters into Controller.Params.
-		r.SessionFilter,           // Restore and write the session cookie.
-		r.FlashFilter,             // Restore and write the flash cookie.
-		r.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		r.I18nFilter,              // Resolve the requested language
-		r.InterceptorFilter,       // Run interceptors around the action.
-		r.ActionInvoker,           // Invoke the action.
+	revel.Filters = []revel.Filter{
+		revel.PanicFilter,             // Recover from panics and display an error page instead.
+		revel.RouterFilter,            // Use the routing table to select the right Action
+		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+		revel.ParamsFilter,            // Parse parameters into Controller.Params.
+		revel.SessionFilter,           // Restore and write the session cookie.
+		revel.FlashFilter,             // Restore and write the flash cookie.
+		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+		revel.I18nFilter,              // Resolve the requested language
+		revel.InterceptorFilter,       // Run interceptors around the action.
+		revel.ActionInvoker,           // Invoke the action.
 	}
 
-	r.OnAppStart(Init)
+	revel.OnAppStart(Init)
 }
